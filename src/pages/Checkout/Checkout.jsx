974ee5c1f84a52136aa7eb628c0b790b355919b0
Asier <asierperez95@gmail.com>
2023-05-24 23:15:40 +0200

    checkout button

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -1,60 +1,66 @@
-import { useContext } from 'react'
-import { CartContext } from '../../context/cart'
-import './Checkout.css'
+import { useContext } from "react";
+import { CartContext } from "../../context/cart";
+import "./Checkout.css";
 
 const Checkout = () => {
-    const { cart, addToCart, removeFromCart, decreaseProductQuantity } = useContext(CartContext)
+  const { cart, addToCart, removeFromCart, decreaseProductQuantity } =
+    useContext(CartContext);
 
-    const totalPrice = cart.reduce((acc, product) => acc + product.price * product.quantity, 0)
+  const totalPrice = cart.reduce(
+    (acc, product) => acc + product.price * product.quantity,
+    0
+  );
 
   return (
     <>
-        <h2>Checkout</h2>
-        
-            <table class='checkout-table'>
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th>Product</th>
-                        <th>Quantity</th>
-                        <th></th>
-                        <th>Price</th>
-                    </tr>
-                </thead>
-                <tbody>
-                {cart.map((product) => (
-                    <tr key={product.id}>
-                        <td className='product-image' >
-                           <img src={product.thumbnail} />
-                        </td>
-                        <td class="product-name">
-                            {product.title}
-                        </td>
-                        <td class="product-quantity">
-                            <button onClick={() => decreaseProductQuantity(product)}>-</button>
-                            <span>{product.quantity}</span>
-                            <button onClick={() => addToCart(product)}>+</button>
-                        </td>
-                        <td className='product-remove'>
-                            <button onClick={() => removeFromCart(product)}>Remove</button>
-                        </td>
-                        <td class="product-price">
-                            {product.price * product.quantity} €
-                        </td>
-                    </tr>
-                ))}
-                </tbody>
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td>Total</td>
-                    <td>{totalPrice} €</td>
-                </tr>
-            </table>
-        
-    </>
-  )
-}
+      <h2>Checkout</h2>
 
-export default Checkout
+      <table class="checkout-table">
+        <thead>
+          <tr>
+            <th></th>
+            <th>Product</th>
+            <th>Quantity</th>
+            <th></th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cart.map((product) => (
+            <tr key={product.id}>
+              <td className="product-image">
+                <img src={product.thumbnail} />
+              </td>
+              <td class="product-name">{product.title}</td>
+              <td class="product-quantity">
+                <button onClick={() => decreaseProductQuantity(product)}>
+                  -
+                </button>
+                <span>{product.quantity}</span>
+                <button onClick={() => addToCart(product)}>+</button>
+              </td>
+              <td className="product-remove">
+                <button onClick={() => removeFromCart(product)}>Remove</button>
+              </td>
+              <td class="product-price">
+                {product.price * product.quantity} €
+              </td>
+            </tr>
+          ))}
+        </tbody>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td>Total</td>
+          <td>{totalPrice} €</td>
+        </tr>
+      </table>
+      <div className="buyButton">
+        <button>Buy Now</button>
+      </div>
+    </>
+  );
+};
+
+export default Checkout;
