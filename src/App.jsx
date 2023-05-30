1af7867beddb01b008dd4f6cf2b06876d74d998c
Asier <asierperez95@gmail.com>
2023-05-30 21:24:02 +0200

    path fix?

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import Checkout from "./pages/Checkout/Checkout";
 import Home from "./pages/Home/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "@stripe/stripe-js";
-import Cancel from "./pages/Cancel";
-import Success from "./pages/Success";
+import Cancel from "./pages/Checkout/Cancel";
+import Success from "./pages/Checkout/Success";
 import "./App.css";
 
 function App() {
@@ -15,9 +15,9 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/cancel" element={<Cancel />} />
-          <Route path="/success" element={<Success />} />
+          <Route path="checkout" element={<Checkout />} />
+          <Route path="cancel" element={<Cancel />} />
+          <Route path="success" element={<Success />} />
         </Routes>
       </BrowserRouter>
 
